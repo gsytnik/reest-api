6e00405d860ad7a936343612dd6506de6850d00d
Gennady Sytnik <gsytnik@calpoly.edu>
2021-01-18 18:05:09 -0800

    id gen works

--- sample_backend.py
+++ sample_backend.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from flask_cors import CORS
+import random
+
+
 app = Flask(__name__)
 CORS(app)
 
@@ -53,9 +56,11 @@ def get_users():
       return users
    elif request.method == 'POST':
       userToAdd = request.get_json()
+      userToAdd["id"] = random.randint(0, 999999)
+      
       users['users_list'].append(userToAdd)
       resp = jsonify(success=True)
-      #resp.status_code = 200 #optionally, you can always set a response code. 
+      resp.status_code = 201 #optionally, you can always set a response code.  
       # 200 is the default code for a normal response
       return resp
    elif request.method == 'DELETE':
--- src/App.js
+++ src/App.js
@@ -39,8 +39,24 @@ class App extends Component {
 	}
 	
 	handleSubmit = character => {
-		this.setState({ characters: [...this.state.characters, character] })
-	};
+   		this.makePostCall(character).then( callResult => {
+      		if (callResult === true) {
+         		this.setState({ characters: [...this.state.characters, character] });
+      		}
+   		});
+ 	}
+
+ 	makePostCall(character){
+   		return axios.post('http://localhost:5000/users', character)
+    		.then(function (response) {
+      		console.log(response);
+      		return (response.status === 201);
+    		})
+    	  .catch(function (error) {
+      		console.log(error);
+      		return false;
+    	});
+ 	}
 
 	render() {
 		const { characters } = this.state;
