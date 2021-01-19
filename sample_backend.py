29da18bbe18abb87bfe193fd57280aba43ca8c70
Gennady Sytnik <gsytnik@calpoly.edu>
2021-01-18 16:36:12 -0800

    testing

---
+++ sample_backend.py
@@ -0,0 +1,98 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello_world():
+	return 'Hello, world!'
+
+users = { 
+   'users_list' :
+   [
+      { 
+         'id' : 'xyz789',
+         'name' : 'Charlie',
+         'job': 'Janitor',
+      },
+      {
+         'id' : 'abc123', 
+         'name': 'Mac',
+         'job': 'Bouncer',
+      },
+      {
+         'id' : 'ppp222', 
+         'name': 'Mac',
+         'job': 'Professor',
+      },
+      {
+         'id' : 'yat999', 
+         'name': 'Dee',
+         'job': 'Aspring actress',
+      },
+      {
+         'id' : 'zap555', 
+         'name': 'Dennis',
+         'job': 'Bartender',
+      }
+   ]
+}
+
+@app.route('/users', methods=['GET', 'POST', 'DELETE'])
+def get_users():
+   if request.method == 'GET':
+      search_username = request.args.get('name')
+      if search_username :
+         subdict = {'users_list' : []}
+         for user in users['users_list']:
+            if user['name'] == search_username:
+               subdict['users_list'].append(user)
+         return subdict
+      return users
+   elif request.method == 'POST':
+      userToAdd = request.get_json()
+      users['users_list'].append(userToAdd)
+      resp = jsonify(success=True)
+      #resp.status_code = 200 #optionally, you can always set a response code. 
+      # 200 is the default code for a normal response
+      return resp
+   elif request.method == 'DELETE':
+      userToRemove = request.get_json()
+      users['users_list'].remove(userToRemove)
+      resp = jsonify(success=True)
+      #resp.status_code = 200 #optionally, you can always set a response code. 
+      # 200 is the default code for a normal response
+      return resp
+
+@app.route('/users/<id>')
+def get_user(id):
+   if id :
+      for user in users['users_list']:
+        if user['id'] == id:
+           return user
+      return ({})
+   return users
+
+@app.route('/jobs/<job>')
+def get_job(job):
+   if job :
+   	  subdict = {'users_list' : []}
+   	  for user in users['users_list']:
+   	  	if user['job'] == job:
+   	  		subdict['users_list'].append(user)
+   	  return subdict
+   	  return ({})
+   return users
+
+@app.route('/names/<name>')
+def get_name(name):
+   if name :
+   	  subdict = {'users_list' : []}
+   	  for user in users['users_list']:
+   	  	if user['name'] == name:
+   	  		subdict['users_list'].append(user)
+   	  return subdict
+   	  return ({})
+   return users
